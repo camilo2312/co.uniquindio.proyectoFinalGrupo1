eeadb113facd0334945988d3683616ed5ae46268
camilo2312 <42982644+camilo2312@users.noreply.github.com>
2021-10-26 09:10:11 -0500

    Se agrega estilos a la vista de Login y las propiedades en el controlador

--- src/co/uniquindio/proyectoFinalGrupo1/controller/LoginController.java
+++ src/co/uniquindio/proyectoFinalGrupo1/controller/LoginController.java
@@ -1,5 +1,29 @@
 package co.uniquindio.proyectoFinalGrupo1.controller;
 
-public class LoginController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
+/**
+ * Controlador de la vista Login
+ * @author Juan Camilo Ramos R
+ *
+ */
+public class LoginController
+{
+
+	@FXML
+    private TextField txtContrasena;
+
+    @FXML
+    private Button btnIngresar;
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    void ingresarAction(ActionEvent event) {
+
+    }
 }
